ff8dcefbd90b8cfec5785129803a6d9282fdb457
Amogh <amogha.kraj@gmail.com>
2018-09-08 15:06:12 -0700

    Add CSS style steet and improve user experience

--- public/js/index.js
+++ public/js/index.js
@@ -18,7 +18,7 @@ socket.on('newMessage', function (message) {
 socket.on('newLocationMessage', function (message){
     var li = jQuery('<li></li>');
     var a = jQuery('<a target ="_blank">My current location</a>');
-    li.text('${message.from}: ');
+    li.text(`${message.from}: `);
     a.attr('href', message.url);
     li.append(a);
     jQuery('#messages').append(li);
@@ -31,6 +31,7 @@ jQuery('#message-form').on('submit', function(e){
         text : jQuery('[name = message]').val()
     }, function (data) {
         console.log('got it!', data);
+        jQuery('[name = message]').val('');
     })
 });
 
@@ -39,12 +40,15 @@ locationButton.on('click', function (){
     if(!navigator.geolocation){
         return alert('Geolocation not supported by your browser');
     }
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition(function (position){
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude : position.coords.latitude,
             longitude : position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch the location');
     });
 });
